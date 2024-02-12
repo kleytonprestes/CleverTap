b4cf5ebe2f6b5e0e0486b1af6cb2d0ff2967357e
Kleyton Prestes <kleytonprestes1@gmail.com>
2024-02-12 10:32:59 -0300

    Initial Commit

---
+++ CleverTap-Test-Kleyton/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CleverTap-Test-Kleyton
+//
+//  Created by kleyton prestes on 12/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
